57f096bb0ac636179c16570b313ac6a7e8c0ee50
OlegEgoism <olegpustovalov220@gmail.com>
2025-01-31 23:17:52 +0300

    ver 15

--- work_db/urls.py
+++ work_db/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html',
                                                                  email_template_name='registration/password_reset_email.html',
+                                                                 html_email_template_name='registration/password_reset_email.html',
                                                                  subject_template_name='registration/password_reset_subject.txt',
                                                                  success_url='/password-reset/done/'
                                                                  ), name='password_reset'),
